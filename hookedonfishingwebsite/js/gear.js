0a838b9018b5d75bd5e629b65c4f0f98f8b0c398
ryantaylor23 <130404736+ryantaylor23@users.noreply.github.com>
2023-10-01 17:58:13 -0600

    Add files via upload

---
+++ hookedonfishingwebsite/js/gear.js
@@ -0,0 +1,42 @@
+"use strict";
+
+$(document).ready( () => {
+  let nextSlide = $("#slide img:first-child");
+
+  const runSlideShow = () => {
+    $("#caption").fadeOut(1000);
+    $("#slide").fadeOut(1000, () => {
+      if (nextSlide.next().length == 0) {
+        nextSlide = $("#slides img:first-child");
+      } 
+      else {
+        nextSlide = nextSlide.next();
+      }
+      const nextSlideSource = nextSlide.attr("src");
+      const nextCaption = nextSlide.attr("alt");
+      $("#slide").attr("src", nextSlideSource).fadeIn(1000);
+      $("#caption").text(nextCaption).fadeIn(1000);
+    }
+    );
+};
+
+   let timer1 = setInterval(runSlideShow, 3000);
+
+   $("#slide").click( () => {
+       if (timer1 != null) {
+       clearInterval(timer1);
+       timer1 = null;
+    }
+     else {
+      runSlideShow();
+      timer1 = setInterval(runSlideShow, 3000);
+    }
+  });
+
+  const links = document.querySelectorAll("img");
+
+  for ( let link of links ) {
+    const image = new Image();
+    image.src = link.href;
+  }
+});
